325a3c420682edeebfa3d0677ba7ff7abfb6a721
Matias P. Brutti <matiasbrutti@gmail.com>
2016-04-18 14:12:51 -0700

    Adding default colors to tables.

--- lib/cartero/command_helpers.rb
+++ lib/cartero/command_helpers.rb
@@ -11,19 +11,19 @@ class CommandHelpers
   def generate_table(p, name)
     cols = Integer(`tput co`) - 20
     table do
-      row do
+      row(:color => 'red') do
         column("Command:", :width => 16, :bold => true)
         column(name, :width => cols)
       end
       p.each do |key,value|
         case value
         when String
-          row do
+          row(:color => 'blue') do
             column(key.to_s.capitalize + ":", :width => 16, :bold => true)
             column(value, :width => cols)
           end
         when Array
-          row do
+          row(:color => 'blue') do
             column(key.to_s.capitalize + ":")
             column(value[0])
           end
@@ -35,12 +35,12 @@ class CommandHelpers
           end
         when Hash
           values = value.to_a
-          row do
+          row(:color => 'blue') do
             column(key.to_s.capitalize + ":")
             column(values[0][0].to_s + " = " + values[0][1].to_s)
           end
           values[1..-1].each do |k,v|
-            row do
+            row(:color => 'blue') do
               column("")
               column(k.to_s + " = " + v.to_s)
             end
